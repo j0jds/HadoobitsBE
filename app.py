5076629c0793b43eccb0ebc6c3b03a5b3e3125d7
asher gabriela <109311931+ashcrysis@users.noreply.github.com>
2024-10-10 19:22:19 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route("/") 
+@app.route("/")
 
 def home():
     return jsonify({"message": "Olá matrizes!"})
 
-@app.route("/teste", methods=["GET"])  
+@app.route("/teste", methods=["GET"])
 def teste():
     return jsonify({"status": "Lorem ipsum lorem lorem hihihi!"})
 
